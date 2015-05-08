6b5e72dcd2e157f2f63b990bec83c3f2948c2c25
Michael Spear <github@bsd4breakfast.us>
2015-05-08 07:53:44 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+	/*create an ajax target to save changes, this should handle login and the initial fetch of data*/
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Kanban</title>
+	<!--  
+	get bootstrap cdn
+	jquery and jquery-ui cdn
+	-->
+</head>
+<body>
+ 
+</body>
+</html> 
